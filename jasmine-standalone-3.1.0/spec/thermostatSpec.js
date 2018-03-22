324cdde2ef5363ccba6d8e63ae175b9fd5f80716
AlexMcCarroll <alexandra@mccarrollhk.com>
2018-03-22 13:44:50 +0000

    added down min max function

--- jasmine-standalone-3.1.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.1.0/spec/thermostatSpec.js
@@ -1,15 +1,24 @@
 describe('Thermostat', function(){
 
   var thermostat = new Thermostat;
-  describe('default temperature', function(){
-    it('is 20 degree', function(){
+  describe('temperature limits', function(){
+    it('default is 20 degrees', function(){
       expect(thermostat.temperature()).toBe(20)
+    });
+    it('minimum is 10 degrees', function(){
+      expect(thermostat.minimum()).toBe(10)
+    });
+    it('maximum is 32 degrees', function(){
+      expect(thermostat.maximum()).toBe(32)
     });
   });
 
   describe('changes the temperature', function(){
     it('increases the temperature', function(){
       expect(thermostat.up(1)).toBe(21)
+    });
+    it('decreases the temperature', function(){
+      expect(thermostat.down(1)).toBe(19)
     });
   });
 });
--- jasmine-standalone-3.1.0/src/thermostat.js
+++ jasmine-standalone-3.1.0/src/thermostat.js
@@ -1,10 +1,25 @@
 function Thermostat() {
-  this._temp = 20
-}
+  this._temp = 20;
+  this._min = 10;
+  this._max = 32;
+};
+
 Thermostat.prototype.temperature = function () {
   return this._temp;
 };
 
+Thermostat.prototype.minimum = function () {
+  return this._min;
+};
+
+Thermostat.prototype.maximum = function () {
+  return this._max;
+};
+
 Thermostat.prototype.up = function (degree) {
   return this._temp + degree;
+};
+
+Thermostat.prototype.down = function (degree) {
+  return this._temp - degree;
 };
