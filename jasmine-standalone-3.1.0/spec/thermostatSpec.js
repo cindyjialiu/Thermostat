1e25fbdac765de9fdab12acf24fc7831b15826cb
Cindy <jl7e12@gmail.com>
2018-03-22 15:41:54 +0000

    added max temp default and power saving mode

--- jasmine-standalone-3.1.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.1.0/spec/thermostatSpec.js
@@ -5,20 +5,41 @@ describe('Thermostat', function(){
     it('default is 20 degrees', function(){
       expect(thermostat.temperature()).toBe(20)
     });
+
     it('minimum is 10 degrees', function(){
       expect(thermostat.minimum()).toBe(10)
-    });
-    it('maximum is 32 degrees', function(){
-      expect(thermostat.maximum()).toBe(32)
     });
   });
 
   describe('changes the temperature', function(){
     it('increases the temperature', function(){
-      expect(thermostat.up(1)).toBe(21)
+      expect(thermostat.up()).toBe(21)
     });
+
     it('decreases the temperature', function(){
-      expect(thermostat.down(1)).toBe(19)
+      expect(thermostat.down()).toBe(19)
+    });
+  });
+
+  describe('power saving mode is on', function(){
+    it('is on by default', function(){
+      expect(thermostat.powerSavingOn()).toEqual(true)
+    });
+
+    it('maximum temperature is 25 degrees', function(){
+      expect(thermostat.maximum()).toBe(25)
+    });
+  });
+
+  describe('power saving mode is off', function(){
+    it('is off', function(){
+      console.log(thermostat.powerSavingOff())
+      expect(thermostat.powerSavingOff()).toEqual(false)
+    });
+
+    it('maximum temperature is 32 degrees', function(){
+      spyOn(thermostat, 'powerSavingOff').and.returnValue(false)
+      expect(thermostat.maximum()).toBe(32)
     });
   });
 });
--- jasmine-standalone-3.1.0/src/thermostat.js
+++ jasmine-standalone-3.1.0/src/thermostat.js
@@ -2,6 +2,8 @@ function Thermostat() {
   this._temp = 20;
   this._min = 10;
   this._max = 32;
+  this._defaultMax = 25;
+  this._powerSav = true
 };
 
 Thermostat.prototype.temperature = function () {
@@ -12,14 +14,29 @@ Thermostat.prototype.minimum = function () {
   return this._min;
 };
 
+Thermostat.prototype.powerSavingOn = function () {
+  return this._powerSav;
+};
+
+Thermostat.prototype.powerSavingOff = function () {
+  if(this._powerSav === true){
+    return this._powerSav = false;
+}else {
+  return false
+}
+};
+
 Thermostat.prototype.maximum = function () {
+  if(this._powerSav === true) {
+    return this._defaultMax;
+  }
   return this._max;
 };
 
-Thermostat.prototype.up = function (degree) {
-  return this._temp + degree;
+Thermostat.prototype.up = function () {
+  return this._temp + 1;
 };
 
-Thermostat.prototype.down = function (degree) {
-  return this._temp - degree;
+Thermostat.prototype.down = function () {
+  return this._temp - 1;
 };
